bd80b8320bbff8b36d29acffb8ccc841d686236c
kasajei <sevenbluesea.yk@gmail.com>
2018-08-31 12:07:04 +0900

    mailer

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,8 @@ User.create(
   password: "foobar",
   password_confirmation: "foobar",
   admin:true,
+  activated: true,
+  activated_at: Time.zone.now,
 )
 
 99.times do |n|
@@ -22,6 +24,8 @@ User.create(
     name:name,
     email:email,
     password:password,
-    password_confirmation:password
+    password_confirmation:password,
+    activated: true,
+    activated_at: Time.zone.now,
   )
 end
