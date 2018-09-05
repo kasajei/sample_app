ad19e1eb27b5fc3ae11453ac2d957bffdd2dd562
kasajei <sevenbluesea.yk@gmail.com>
2018-09-05 17:41:30 +0900

    show following / followers

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -81,4 +81,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     end
     assert_redirected_to root_url
   end
+
+
+  
 end
--- test/integration/users_profile_test.rb
+++ test/integration/users_profile_test.rb
@@ -18,5 +18,15 @@ class UsersProfileTest < ActionDispatch::IntegrationTest
     @user.microposts.paginate(page: 1).each do |micropost|
       assert_match micropost.content, response.body
     end
+    assert_match @user.following.count.to_s, response.body
+    assert_match @user.followers.count.to_s, response.body
+  end
+
+  test "home page display" do
+    log_in_as(@user)
+    get root_url
+    assert_template "static_pages/home"
+    assert_match @user.following.count.to_s, response.body
+    assert_match @user.followers.count.to_s, response.body
   end
 end
